cd21943e88158ab6519f36f0ce479695f4c69752
Logical Groove <info@logicalgroove.com>
2017-04-20 22:28:26 +0000

    add server-side functionality

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -23,6 +23,7 @@ class ChatRoomsController < ApplicationController
   end
 
   private
+  #########################################################################
 
   def chat_room_params
     params.require(:chat_room).permit(:title)
