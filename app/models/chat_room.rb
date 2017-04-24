74ce1b30bc63de7cb36419df698da174aa74bcc1
Logical Groove <info@logicalgroove.com>
2017-04-24 07:49:12 +0000

    fix for messages order

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,4 +1,4 @@
 class ChatRoom < ApplicationRecord
   has_many :users, -> { uniq }, through: :messages
-  has_many :messages, dependent: :destroy
+  has_many :messages, -> { order(created_at: :asc) }, dependent: :destroy
 end
